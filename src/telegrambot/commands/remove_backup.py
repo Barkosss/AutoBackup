8143386556a4b25aa1e5285cbb7101846ebfd55c
Barkosss <andrey.bar66@gmail.com>
2025-07-20 19:10:20 +0500

    Add method for remove backup

--- src/telegrambot/commands/remove_backup.py
+++ src/telegrambot/commands/remove_backup.py
@@ -1,17 +1,26 @@
 from telegram import Update
-from telegram.ext import ContextTypes
+from telegram.ext import ContextTypes, ConversationHandler
 
 from .base_command import BaseCommand
 
+WAITING_FOR_NAME = 1
 
 class RemoveBackupCommand(BaseCommand):
     def get_name(self) -> str:
         return "remove_backup"
 
     def get_description(self) -> str:
-        return "Удалить резервное копирование"
+        return "Delete Backup"
 
-    async def execute(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    async def execute(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         assert update.message is not None
 
         await update.message.reply_text("Remove backup")
+        return WAITING_FOR_NAME
+
+    @staticmethod
+    async def remove(update: Update, backup_name: str) -> int:
+        assert update.message is not None
+
+        await update.message.reply_text(f"Text={backup_name}")
+        return ConversationHandler.END
