ca5af11773cd794862e9fcd7567a81341e1b7cae
Barkosss <andrey.bar66@gmail.com>
2025-07-20 18:38:43 +0500

    Release command "list_backup"

--- src/telegrambot/commands/list_backup.py
+++ src/telegrambot/commands/list_backup.py
@@ -1,4 +1,7 @@
+import os
+
 from telegram import Update
+from telegram.constants import ParseMode
 from telegram.ext import ContextTypes
 
 from .base_command import BaseCommand
@@ -9,9 +12,22 @@ class ListBackupCommand(BaseCommand):
         return "list_backup"
 
     def get_description(self) -> str:
-        return "Получить список копий"
+        return "Get a list of copies"
 
     async def execute(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         assert update.message is not None
 
-        await update.message.reply_text("List backup")
+        backups: list[str] = self._get_backups()
+        message: str = "====== LIST ======\n\n"
+        for index, file_name in enumerate(backups):
+            message += f"{index + 1}. {file_name}\n"
+        await update.message.reply_text(message)
+
+    @staticmethod
+    def _get_backups() -> list[str]:
+        backups: list[str] = []
+        with os.scandir(os.getenv("BACKUP_DIRECTORY")) as entries:
+            for entry in entries:
+                if entry.is_file():
+                    backups.append(entry.name)
+        return backups
